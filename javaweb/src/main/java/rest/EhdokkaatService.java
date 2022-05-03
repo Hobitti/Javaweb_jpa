3df9a83c20853ee7ac0c17fc08280bdca25520e2
stlgithub <sami.lappalainen@hotmail.fi>
2022-05-03 13:50:53 +0300

    Read & Delete toimii

--- javaweb/src/main/java/rest/EhdokkaatService.java
+++ javaweb/src/main/java/rest/EhdokkaatService.java
@@ -1,52 +1,83 @@
 package rest;
- import java.util.List;
+import java.io.IOException;
+import java.util.List;
 
- import javax.persistence.EntityManager;
- import javax.persistence.EntityManagerFactory;
- import javax.persistence.Persistence;
- import javax.ws.rs.Consumes;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
- import javax.ws.rs.Path;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
- import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
- import dao.Dao;
- import data.Ehdokas;
+import dao.Dao;
+import data.Ehdokas;
 
- @Path("/ehdokasService")
- public class EhdokkaatService {
- 	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpavaalikone");
+@Path("/ehdokasService")
+public class EhdokkaatService {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpavaalikone");
 
- 	@GET
- 	@Path("/readEhdokkaat")
- 	@Produces(MediaType.APPLICATION_JSON)
- 	@Consumes(MediaType.APPLICATION_JSON)
- 	public List<Ehdokas> readEhdokkaat() {
- 		EntityManager em = emf.createEntityManager();
- 		em.getTransaction().begin();
- 		List<Ehdokas> ehdokkaat = em.createQuery("SELECT a FROM Ehdokas a").getResultList();
- 		em.getTransaction().commit();
- 		System.out.println(ehdokkaat.get(0).getNimi());
- 		return ehdokkaat;
- 	}
- 	
+	@GET
+	@Path("/readEhdokkaat")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Ehdokas> readEhdokkaat() {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		List<Ehdokas> ehdokkaat = em.createQuery("SELECT a FROM Ehdokas a").getResultList();
+		em.getTransaction().commit();
+		System.out.println(ehdokkaat.get(0).getNimi());
+		return ehdokkaat;
+	}
 	@DELETE
 	@Path("/deleteEhdokas/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public List<Ehdokas> deleteEhdokas(@PathParam("id") int id) {
+	public List<Ehdokas> deleteFish(@PathParam("id") int id) {
 		EntityManager em=emf.createEntityManager();
 		em.getTransaction().begin();
-		Ehdokas e=em.find(Ehdokas.class, id);
-		if (e!=null) {
-			em.remove(e);//The actual insertion line
+		Ehdokas f=em.find(Ehdokas.class, id);
+		if (f!=null) {
+			em.remove(f);//The actual delete line
 		}
 		em.getTransaction().commit();
-		//Calling the method readEhdokkaat() of this service
+		//Calling the method readEhdokas() of this service
 		List<Ehdokas> list=readEhdokkaat();		
 		return list;
 	}	
- 	
- }
+	@GET
+	@Path("/deleteEhdokas/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void deleteEhdokasByGet(@PathParam("id") int id, 
+			@Context HttpServletRequest request,
+			@Context HttpServletResponse response
+			) {
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		Ehdokas f=em.find(Ehdokas.class, id);
+		if (f!=null) {
+			em.remove(f);//The actual delete line
+		}
+		em.getTransaction().commit();
+		//Calling the method readEhdokas() of this service
+		List<Ehdokas> list=readEhdokkaat();		
+		
+//		RequestDispatcher rd=request.getRequestDispatcher("/jsp/ehdokasform.jsp");
+//		request.setAttribute("ehdokaslist", list);
+//		try {
+//			rd.forward(request, response);
+//		} catch (ServletException | IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+}
